7b8484b441e799ef9004bd595cc84c3be1963728
Kris Gainsforth <kris.g@xoticpc.com>
2017-08-15 10:12:57 -0500

    added JSON file and started middle logic

---
+++ ptsfeconnect.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" logic to interact with phoronix-test-suite """
+
+import os.path
+
+
+
+
+class ptsfefile_ospath():
+    def check_json_exists():
+        if os.path.isfile('./ptsfe.json') = 'True':
