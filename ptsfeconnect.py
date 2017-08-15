12b594d28c847cdbf92a012727138c8fd6f820aa
Kris Gainsforth <kris.g@xoticpc.com>
2017-08-15 11:34:40 -0500

    cleanup

--- ptsfeconnect.py
+++ ptsfeconnect.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 """ logic to interact with phoronix-test-suite """
 
-import os.path
-
-
-
-
-class ptsfefile_ospath():
-    def check_json_exists():
-        if os.path.isfile('./ptsfe.json') = 'True':
+class PTSgo(self):
+    
