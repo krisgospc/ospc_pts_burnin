3764cfe660acf69feb4060ecae8be843478c6263
Kris Gainsforth <kris.g@xoticpc.com>
2017-08-21 10:27:30 -0500

    added basic JSON read/write

--- ospcpts/ptsrun.py
+++ ospcpts/ptsrun.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 """ logic to interact with phoronix-test-suite """
 
-import 
+import json
+
+#basic JSON write
+with open('filename.json', 'w') as f:
+    json.dump(data, f)
+
+#basic JSON read
+with open('filename.json', 'r') as r:
+    data_struct = json.load(f)
